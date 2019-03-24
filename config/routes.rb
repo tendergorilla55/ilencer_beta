afc19485560a0228ec606665d5bb3a1971660814
Miki Tatsuya <gorilladunk.tatsuya@gmail.com>
2019-03-24 16:50:02 +0900

    Delete demo01
    不要なでもファイルの削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   root 'users#index'
   resources :users, only: [:index]
-  resources :demos, only: [:index] do
-    collection do
-      get 'demo01'
-      get 'demo02'
-    end
-  end
+  resources :demos, only: [:index]
 end
