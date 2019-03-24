00ec27724b45b32f016a599be6cd61f6eb523a61
Miki Tatsuya <gorilladunk.tatsuya@gmail.com>
2019-03-24 16:37:09 +0900

    Add button jquery
    ボタンを押すと色が変わる

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root 'users#index'
-  resources :users
+  resources :users, only: [:index]
+  resources :demos, only: [:index] do
+    collection do
+      get 'demo01'
+      get 'demo02'
+    end
+  end
 end
